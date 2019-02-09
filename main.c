3123ffbbfdac3c08a7e2185e07d18d98bbf9906b
ricky_daniel13 <danykun.echidna@gmail.com>
2019-02-09 10:16:36 -0600

    Inserte mi funcion

--- main.c
+++ main.c
@@ -2,16 +2,21 @@
 /* 
  *	Nombre	Comentario
  *	Omar 	Agregue la funcion HolaSoyOmar
- *
+ *      Daniel G Agregue La funcion HolaSoyGabogod
  * */
 
 void HolaSoyOmar()
 {
 	printf("Hola mi nombre es Omar Alvizo\n");
-}	
+}
+void HolaSoyGabogod()
+{
+	printf("Hola, soy Daniel Garibaldi");
+}
 
 int main()
 {
 	HolaSoyOmar();
+	HolaSoyGabogod();
 	return 0;
 }
