ec2d8334e27b7bbf114dd85079f4759150661393
DiegoRubio1523 <diegodarc1999@hotmail.com>
2019-02-09 10:20:31 -0600

    Agregue funcion HolaSoyDiegoR

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 /* 
  *	Nombre	Comentario
  *	Omar 	Agregue la funcion HolaSoyOmar
- *
+ *	Diego Alejandro Rubio Contreras Agregue la funcion HolaSoyDiegoRubio
  * */
 
 void HolaSoyOmar()
@@ -10,8 +10,15 @@ void HolaSoyOmar()
 	printf("Hola mi nombre es Omar Alvizo\n");
 }	
 
+void HolaSoyDiegoRubio()
+{
+	printf("Hola mi nombre es Diego Rubio\n");
+}	
+
+
 int main()
 {
 	HolaSoyOmar();
+	HolaSoyDiegoRubio();
 	return 0;
 }
