879b080d9d2a78c677896ea870b7482e7d254749
lesliejocelyn6 <lmm_005-leslie@hotmail.com>
2019-02-09 10:17:46 -0600

    Leslie Morales

--- main.c
+++ main.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
-/* 
- *	Nombre	Comentario
- *	Omar 	Agregue la funcion HolaSoyOmar
+/*
+ *	Leslie Jocelyn Morales Mendoza
+ *	Leslie 	Agregue la funcion HolaSoyLeslie
  *
  * */
 
 void HolaSoyOmar()
 {
 	printf("Hola mi nombre es Omar Alvizo\n");
-}	
+}
+
+void HolaSoyLeslie()
+{
+	printf("Hola mi nombre es Leslie Jocelyn Morales Mendoza\n");
+}
 
 int main()
 {
 	HolaSoyOmar();
+	HolaSoyLeslie();
 	return 0;
 }
