fe26ca4b7c05385c323578a092f2120ee84262e8
Sujey Galvan <alfred.ramon.nieves@gmail.com>
2019-02-09 10:41:40 -0600

    Agregue mi nombre

--- main.c
+++ main.c
@@ -2,16 +2,22 @@
 /* 
  *	Nombre	Comentario
  *	Omar 	Agregue la funcion HolaSoyOmar
- *
+ *	Alfred	Agregué mi nombre
  * */
 
 void HolaSoyOmar()
 {
 	printf("Hola mi nombre es Omar Alvizo\n");
+}
+
+void HolaSoyAlfred()
+{
+	printf("Hola mi nombre es Alfred Nieves\n");
 }	
 
 int main()
 {
 	HolaSoyOmar();
+	HolaSoyAlfred();
 	return 0;
 }
