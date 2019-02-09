16c79d414e525cf14f956f7411946a87352cc2fe
unknown <esme_503@outlook.com>
2019-02-09 10:18:38 -0600

    Añadi mi nombre y funciones

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 /* 
  *	Nombre	Comentario
  *	Omar 	Agregue la funcion HolaSoyOmar
- *
+ *	Ana 	agregue mi nombre
  * */
 
 void HolaSoyOmar()
@@ -10,8 +10,14 @@ void HolaSoyOmar()
 	printf("Hola mi nombre es Omar Alvizo\n");
 }	
 
+void HolaSoyAna()
+{
+	printf("Hola mi nombre es Ana Gozalez\n");
+}
+
 int main()
 {
 	HolaSoyOmar();
+	HolaSoyAna();
 	return 0;
 }
