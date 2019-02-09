3f51df1727c5e9aa8f5f821c00a3998e5c7323f9
Zaikron <anthony.sandoval1805@gmail.com>
2019-02-09 10:31:31 -0600

    Agregue mi funcion

--- main.c
+++ main.c
@@ -3,15 +3,25 @@
  *	Nombre	Comentario
  *	Omar 	Agregue la funcion HolaSoyOmar
  *
+ *	Diego 	Agregue la funcion HolaSoyDiegoG
+ *
  * */
 
 void HolaSoyOmar()
 {
 	printf("Hola mi nombre es Omar Alvizo\n");
-}	
+}
+
+void HolaSoyDiegoG()
+{
+        printf("Hola mi nombre es Diego Garcia\n");
+}
+
+
 
 int main()
 {
 	HolaSoyOmar();
+	HolaSoyDiegoG();
 	return 0;
 }
