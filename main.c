25e34b95e40ebb6a9a9bac9a59ef9bd8b0030b65
Omar Alvizo <omar.alvizo@alumno.udg.mx>
2019-02-09 09:51:10 -0600

    Cree archivo main.c

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/* 
+ *	Nombre	Comentario
+ *	Omar 	Agregue la funcion HolaSoyOmar
+ *
+ * */
+
+void HolaSoyOmar()
+{
+	printf("Hola mi nombre es Omar Alvizo\n");
+}	
+
+int main()
+{
+	HolaSoyOmar();
+	return 0;
+}
