31384c099572421023fbb531f23d5a0217884288
DanielDiaz18 <dani.diaz.g.32@gmail.com>
2019-02-09 10:21:13 -0600

    agregando funcion HolaSoyDanielDiaz

--- main.c
+++ main.c
@@ -2,16 +2,25 @@
 /* 
  *	Nombre	Comentario
  *	Omar 	Agregue la funcion HolaSoyOmar
+ *      Daniel Diaz agregue  la funcion HolaSoyDanielDiaz
  *
  * */
 
 void HolaSoyOmar()
 {
 	printf("Hola mi nombre es Omar Alvizo\n");
-}	
+}
+
+void HolaSoyDanielDiaz()
+{
+        printf("Hola mi nombre es Daniel Diaz\n");
+}
+	
 
 int main()
 {
 	HolaSoyOmar();
+	HolaSoyDanielDiaz();
 	return 0;
 }
+
