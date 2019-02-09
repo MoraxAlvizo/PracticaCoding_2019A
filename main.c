6d60b69f13b7bd5341ebb62eb322bb4169f41e73
Jesus Alejandro <tormenta_dearena@hotmail.com>
2019-02-09 10:24:53 -0600

    Jesus Alejandro

--- main.c
+++ main.c
@@ -2,16 +2,23 @@
 /* 
  *	Nombre	Comentario
  *	Omar 	Agregue la funcion HolaSoyOmar
+ *	Jesus 	Agregue la funcion HolaSoyJesus
  *
  * */
 
 void HolaSoyOmar()
 {
 	printf("Hola mi nombre es Omar Alvizo\n");
-}	
+}
+
+void HolaSoyJesus()
+{
+	printf("Hola mi nombre es Jesus Garcia\n");
+}		
 
 int main()
 {
 	HolaSoyOmar();
+	HolaSoyJesus();
 	return 0;
 }
