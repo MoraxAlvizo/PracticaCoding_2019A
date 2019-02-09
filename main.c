dff42a8f157179bb60812f2e3fc906df67de3460
Alfred Nieves <alfred.ramon.nieves@gmail.com>
2019-02-09 10:50:15 -0600

    agregue mi funcion

--- main.c
+++ main.c
@@ -3,15 +3,21 @@
  *	Nombre	Comentario
  *	Omar 	Agregue la funcion HolaSoyOmar
  *
+ *  	Turanzas Agregue la funcion HolaSoyTuranzas
  * */
 
 void HolaSoyOmar()
 {
 	printf("Hola mi nombre es Omar Alvizo\n");
 }	
+void HolaSoyTuranzas()
+{
+        printf("Hola mi nombre es Andrea Turanzas\n");
+}
 
 int main()
 {
 	HolaSoyOmar();
+	HolaSoyTuranzas();
 	return 0;
 }
