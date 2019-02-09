93e9a371970933e595f2836ac551f435e2574882
deniseGF <denise.gf911@gmail.com>
2019-02-09 10:09:43 -0600

    Añadi mi nombre y mis funciones

--- main.c
+++ main.c
@@ -2,16 +2,22 @@
 /* 
  *	Nombre	Comentario
  *	Omar 	Agregue la funcion HolaSoyOmar
- *
+ *	Andrea  Agregue mi nombre
  * */
 
 void HolaSoyOmar()
 {
 	printf("Hola mi nombre es Omar Alvizo\n");
+}
+
+void HolaSoyAndrea()
+{
+	printf("Hola mi nombre es Andrea Denise\n");
 }	
 
 int main()
 {
 	HolaSoyOmar();
+	HolaSoyAndrea();
 	return 0;
 }
