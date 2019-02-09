81b371189900295212850d68a6528ee074320c0c
Paula Ramirez <paula.sofyrmz@gmail.com>
2019-02-09 10:26:01 -0600

    Agrege mi funcion

--- main.c
+++ main.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
-/* 
+/*
  *	Nombre	Comentario
  *	Omar 	Agregue la funcion HolaSoyOmar
- *
+ *  Paula Sofia Ramirez Angel Hize la funcion de PaulaRamirez
  * */
 
 void HolaSoyOmar()
 {
 	printf("Hola mi nombre es Omar Alvizo\n");
-}	
+}
+
+void PaulaRamirez()
+{
+    printf("No se programar :c\n");
+}
 
 int main()
 {
 	HolaSoyOmar();
+	PaulaRamirez();
 	return 0;
 }
