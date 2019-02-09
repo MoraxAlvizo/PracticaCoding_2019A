97c9a3fd8e8a0bbbba7958cc8186d72a170cc7a9
000225 <vr2890573@gmail.com>
2019-02-09 10:35:55 -0600

    Victor Ezequiel

--- main.c
+++ main.c
@@ -2,16 +2,21 @@
 /* 
  *	Nombre	Comentario
  *	Omar 	Agregue la funcion HolaSoyOmar
- *
+ *	Ezequiel Agregue la funcion HolasoyEzequiel
  * */
 
 void HolaSoyOmar()
 {
 	printf("Hola mi nombre es Omar Alvizo\n");
+}
+void HolasoyEzequiel()
+{
+	printf("Hola mi nombre es Ezequiel De la Torre\n");
 }	
 
 int main()
 {
 	HolaSoyOmar();
+	HolasoyEzequiel();
 	return 0;
 }
