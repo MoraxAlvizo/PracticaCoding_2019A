4c247fa8b1514b636d8a026cd0ff2145b50bb345
hector7u7 <hectoreduardo7u7@gmail.com>
2019-02-09 10:28:50 -0600

    Andre Calderon

--- main.c
+++ main.c
@@ -2,16 +2,20 @@
 /* 
  *	Nombre	Comentario
  *	Omar 	Agregue la funcion HolaSoyOmar
- *
+ *	Omar 	Agregue la funcion HolaSoyAndreCalderon
  * */
 
 void HolaSoyOmar()
 {
 	printf("Hola mi nombre es Omar Alvizo\n");
 }	
-
+void HolaSoyAndreCalderon()
+{
+	printf("Hola mi nombre es Andre Calderon\n");
+}
 int main()
 {
+                HolaSoyAndreCalderon();
 	HolaSoyOmar();
 	return 0;
 }
