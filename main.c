ac2c3b976ede5e4d9c780073a1e37c42d15ca4d7
SujeyReginaGalvanAlonso <sujeygalvan03@gmail.com>
2019-02-09 10:39:10 -0600

    XD

--- main.c
+++ main.c
@@ -2,16 +2,21 @@
 /* 
  *	Nombre	Comentario
  *	Omar 	Agregue la funcion HolaSoyOmar
- *
+ *	
+ *	Antonio Leaños y la verdad es que no soy gay
  * */
 
 void HolaSoyOmar()
 {
 	printf("Hola mi nombre es Omar Alvizo\n");
 }	
-
+ void HolaMundo()
+{
+ printf("Hola me llamo Ñaños y soy gay XD");
+}
 int main()
 {
 	HolaSoyOmar();
+	HolaMundo();
 	return 0;
 }
