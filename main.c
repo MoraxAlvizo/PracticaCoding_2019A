fe69e830a3b0ed32209280d9f1c329925922a1a3
David <narut16@hotmail.com>
2019-02-09 10:12:27 -0600

    Soy David

--- main.c
+++ main.c
@@ -2,16 +2,22 @@
 /* 
  *	Nombre	Comentario
  *	Omar 	Agregue la funcion HolaSoyOmar
+ *	David	Agregue la función HolaSoyDavid	
  *
  * */
 
 void HolaSoyOmar()
 {
 	printf("Hola mi nombre es Omar Alvizo\n");
+}
+
+void HolaSoyDavid{
+	printf("Hola, mi nombre es David González\n");
 }	
 
 int main()
 {
 	HolaSoyOmar();
+	HolaSoyDavid();
 	return 0;
 }
