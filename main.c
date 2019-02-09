a09b429f8e9403ac0b927f7202975cfb0fab7cf6
diegoivan1987 <diegoivan1987@outlook.com>
2019-02-09 10:15:30 -0600

    agrege mi nombre como funcion y comentario

--- main.c
+++ main.c
@@ -2,16 +2,23 @@
 /* 
  *	Nombre	Comentario
  *	Omar 	Agregue la funcion HolaSoyOmar
+ *	Diego Ivan Agregue la funcion HolaSoyDiegoI
  *
  * */
 
 void HolaSoyOmar()
 {
 	printf("Hola mi nombre es Omar Alvizo\n");
+}
+
+void HolaSoyDiegoI()
+{
+	printf("Hola mi nombre en Diego Ivan\n");
 }	
 
 int main()
 {
 	HolaSoyOmar();
+	HolaSoyDiegoI();
 	return 0;
 }
