18d1d249f10ec7fe1e1339836140a3f741604a74
daniel <danykun.echidna@gmail.com>
2019-02-09 10:38:02 -0600

    holaSoyhector

--- main.c
+++ main.c
@@ -2,7 +2,9 @@
 /* 
  *	Nombre	Comentario
  *	Omar 	Agregue la funcion HolaSoyOmar
+ *	
  *
+ *	Hector agregue la funcion HolaSoyHector
  * */
 
 void HolaSoyOmar()
@@ -10,8 +12,15 @@ void HolaSoyOmar()
 	printf("Hola mi nombre es Omar Alvizo\n");
 }	
 
+void HolaSoyHector()
+{
+        printf("Hola mi nombre es Hector\n");
+}
+
+
 int main()
 {
 	HolaSoyOmar();
+	HolaSoyHector();
 	return 0;
 }
