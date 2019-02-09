bd70946bdf527dd5911f55db4d43966c8f06b33c
unknown <issaqui_gohan@hotmail.com>
2019-02-09 10:24:13 -0600

    Im Tremendo

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 /* 
  *	Nombre	Comentario
  *	Omar 	Agregue la funcion HolaSoyOmar
- *
+ *	Francisco Agregué la función ImTremendo
  * */
 
 void HolaSoyOmar()
@@ -10,8 +10,16 @@ void HolaSoyOmar()
 	printf("Hola mi nombre es Omar Alvizo\n");
 }	
 
+void HolaSoyIsaac()
+{
+	printf("HOLA MI NOMBRE ES: Fracisco Isaac\n");
+}
+
+
+
 int main()
 {
 	HolaSoyOmar();
+	HolaSoyIsaac();
 	return 0;
 }
