861507a77b2c0b6a8b5bc7d8b303b987641e4c0b
Salvi1 <narut16@hotmail.com>
2019-02-09 10:27:16 -0600

    Mi nombre en codigo

--- main.c
+++ main.c
@@ -2,16 +2,23 @@
 /* 
  *	Nombre	Comentario
  *	Omar 	Agregue la funcion HolaSoyOmar
- *
+ *	Juan	Agregue la Funcion HolaSoyJuan
  * */
 
 void HolaSoyOmar()
 {
 	printf("Hola mi nombre es Omar Alvizo\n");
+}
+
+void HolaSoyJuan()
+{
+	printf("Hola Mi nombre es Juan\n");
 }	
 
 int main()
 {
 	HolaSoyOmar();
+	HolaSoyJuan();
+
 	return 0;
 }
