89625b1f3ffea7d2bb42272e8bac3cdb4c2c7b0c
Erick Rodriguez <erickrodriguez0017@gmail.com>
2019-02-09 10:40:23 -0600

    holasoyerick

--- main.c
+++ main.c
@@ -2,16 +2,20 @@
 /* 
  *	Nombre	Comentario
  *	Omar 	Agregue la funcion HolaSoyOmar
- *
- * */
+ *	Erick          Agregue la funcion holasoyerick
+ * */ 
 
 void HolaSoyOmar()
 {
 	printf("Hola mi nombre es Omar Alvizo\n");
-}	
+}
+void holasoyerick(){
+printf ("hola mi nombre es Erick");
+	}
 
 int main()
 {
 	HolaSoyOmar();
+	holasoyerick();
 	return 0;
 }
