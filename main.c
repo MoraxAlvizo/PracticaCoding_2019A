a4bfb28be5a8be522929b9d54678547546df782f
Piloalucard <piloalucard@gmail.com>
2019-02-09 10:14:22 -0600

    Gustavo

--- main.c
+++ main.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
-/* 
+/*
  *	Nombre	Comentario
  *	Omar 	Agregue la funcion HolaSoyOmar
- *
+ *  Gustavo Aguegue la funcion Holasoytavo
  * */
 
 void HolaSoyOmar()
 {
 	printf("Hola mi nombre es Omar Alvizo\n");
-}	
+}
+void HolaSoyTavo()
+{
+	printf("Hola mi nombre es Gustavo Padilla y yo");
+	printf("Le robe a mama de su cartera\n");
+}
 
 int main()
 {
 	HolaSoyOmar();
+	HolaSoyTavo();
 	return 0;
 }
