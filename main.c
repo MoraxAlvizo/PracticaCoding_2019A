d71cc31a51dc6d499f85b66f5bc7881b841987c8
Pedro Lopez Sierra <pedro.lopez010900@gmail.com>
2019-02-09 10:50:43 -0600

    Sydney y soy terroris
    rjgbaijdgba
    fdaubi

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
  *	Nombre	Comentario
  *	Omar 	Agregue la funcion HolaSoyOmar
  *
+ * 	sydney agregue la funcion holaSoySydney
  * */
 
 void HolaSoyOmar()
@@ -10,8 +11,14 @@ void HolaSoyOmar()
 	printf("Hola mi nombre es Omar Alvizo\n");
 }	
 
+void HolaSoySydney()
+{
+        printf("Hola mi nombre es Sydney Lopez\n");
+}
+
 int main()
 {
 	HolaSoyOmar();
+	HolaSoySydney();
 	return 0;
 }
